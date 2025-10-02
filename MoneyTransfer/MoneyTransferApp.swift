e2a9cead27a62493fc74720fe9235d67b6f36c57
Michał Fereniec <michalkoks99@gmail.com>
2025-10-02 09:19:55 +0200

    Initial Commit

---
+++ MoneyTransfer/MoneyTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTransferApp.swift
+//  MoneyTransfer
+//
+//  Created by Michal Fereniec on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoneyTransferTests/MoneyTransferTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTransferTests.swift
+//  MoneyTransferTests
+//
+//  Created by Michal Fereniec on 02/10/2025.
+//
+
+import Testing
+@testable import MoneyTransfer
+
+struct MoneyTransferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
